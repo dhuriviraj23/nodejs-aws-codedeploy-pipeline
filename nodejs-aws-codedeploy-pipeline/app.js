be9ab8b7154aea1162abd53898ba657f3c929451
dhuriviraj23 <92348279+dhuriviraj23@users.noreply.github.com>
2023-09-11 16:41:00 +0530

    Update app.js

--- nodejs-aws-codedeploy-pipeline/app.js
+++ nodejs-aws-codedeploy-pipeline/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
-const version = '2';
+const version = '1';
 const version = '3,000,000';
 
 app.get('/', (req, res) => {
